2c81dc8dd67530b09188a712c50794dfb2fb9efe
Shantanu Tripathi <shantanutripathi96@gmail.com>
2023-04-21 17:41:21 +0530

    complete app with translation

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const btnTranlate = document.querySelector("#btn-translate");
 const txtInput = document.querySelector("#txt-input");
 const outputDiv = document.querySelector("#output");
+const serverUrl = "https://api.funtranslations.com/translate/minion.json"
+// const serverUrl = "https://lessonfourapi.tanayratap.repl.co/translate/yoda.json"
 
-btnTranlate.addEventListener("click",()=>{
-    outputDiv.innerText = txtInput.value
-    fetch("https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=Hello my name is shantanu")
-    .then( (res) => {return res.json(); })
-    .then(json => {
-        console.log(json);
+function getTranslationUrl(text){
+    return serverUrl + "?text=" + text;
+}
+
+function errorHandler(error){
+    console.log("Error occured "+error);
+}
+
+function clickHandler(){
+    const input = txtInput.value;
+
+    fetch(getTranslationUrl(input))
+    .then( response => response.json())
+    .then( json => {
+        outputDiv.innerText = json.contents.translated;
     })
-})
+    .catch(errorHandler);
+}
+
+btnTranlate.addEventListener("click",clickHandler);
